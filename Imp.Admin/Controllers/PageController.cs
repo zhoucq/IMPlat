effd6d07c49ceaf340640ad31d4d8f6256acd2c3
flyontheair <flyontheair@sina.cn>
2014-09-02 17:58:35 +0800

    补上PageController.cs

---
+++ Imp.Admin/Controllers/PageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Imp.Admin.Controllers
+{
+    public class PageController : Controller
+    {
+        //
+        // GET: /Page/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 测试母版页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TestLayout()
+        {
+            return View();
+        }
+
+    }
+}
