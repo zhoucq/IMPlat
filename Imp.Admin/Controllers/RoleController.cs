2243d8b5ba97eb6e1ef30b51e8139680c15334f5
zhoucq <zcq0328@gmail.com>
2014-09-09 13:58:46 +0800

    Role List页面

---
+++ Imp.Admin/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Imp.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
