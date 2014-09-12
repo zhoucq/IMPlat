6732fc1039501fdcf0669b8e02f58988fa9ab8ac
zhoucq <zcq0328@gmail.com>
2014-09-12 18:19:43 +0800

    add DocController

---
+++ Imp.Admin/Controllers/DocController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Imp.Admin.Controllers
+{
+    public class DocController : Controller
+    {
+        //
+        // GET: /Doc/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("List", "Doc");
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
